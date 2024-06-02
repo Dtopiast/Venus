633ac2155991cda183c438e32f458bffe6817e95
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-06-02 16:59:26 -0600

    Add repository service with specification pattern

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -2,12 +2,13 @@ package repository;
 
 import com.dtopiast.venus.model.base.MyModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * A generic repository interface for accessing and managing entities that extend MyModel.
  *
  * @param <TEntity> The type of entities managed by this repository, extending MyModel.
  */
-public interface Repository<TEntity extends MyModel> extends JpaRepository<TEntity, Long> {
+public interface Repository<TEntity extends MyModel> extends JpaRepository<TEntity, Long>, JpaSpecificationExecutor<TEntity> {
 
 }
