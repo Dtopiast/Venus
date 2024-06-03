b269cc5ad114a48e57aad46146ba4253fbda51b2
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-06-03 09:12:07 -0600

    Add ErrorHandler

---
+++ src/main/java/com/dtopiast/venus/controller/error/ErrorHandler.java
@@ -0,0 +1,43 @@
+package com.dtopiast.venus.controller.error;
+
+import com.dtopiast.venus.core.exception.IntegrityBreakException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> Error404Handler() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DataErrorValidation>> Error400Handler(MethodArgumentNotValidException e) {
+        var fails = e.getFieldErrors().stream().map(DataErrorValidation::new).toList();
+        return ResponseEntity.badRequest().body(fails);
+    }
+
+    @ExceptionHandler(IntegrityBreakException.class)
+    public ResponseEntity<String> IntegrityBreakExceptionHandler(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> ValidationExceptionHandler(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    public record DataErrorValidation(String campo, String error) {
+        public DataErrorValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
