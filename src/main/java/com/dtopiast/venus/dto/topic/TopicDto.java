6d4a9a17438a38d2414c2785b36aa0ffb44925de
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-06-02 19:31:54 -0600

    Fix author property type in TopicDto

--- src/main/java/com/dtopiast/venus/dto/topic/TopicDto.java
+++ src/main/java/com/dtopiast/venus/dto/topic/TopicDto.java
@@ -1,12 +1,17 @@
 package com.dtopiast.venus.dto.topic;
 
+import com.dtopiast.venus.dto.user.UserDto;
 import com.dtopiast.venus.model.TopicStatus;
 import com.dtopiast.venus.model.User;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public record TopicDto(String title, String message, TopicStatus status, LocalDate creationDate, User author,
+public record TopicDto(String title,
+                       String message,
+                       TopicStatus status,
+                       LocalDate creationDate,
+                       UserDto author,
                        List<Long> responses) {
 
 }
