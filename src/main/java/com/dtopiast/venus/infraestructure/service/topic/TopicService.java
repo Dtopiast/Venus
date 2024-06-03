10daf7b9ea1b443b5d7dc68931a28b1f9e5127c4
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-06-03 14:38:03 -0600

    Add blank services implementations

---
+++ src/main/java/com/dtopiast/venus/infraestructure/service/topic/TopicService.java
@@ -0,0 +1,55 @@
+package com.dtopiast.venus.infraestructure.service.topic;
+
+import com.dtopiast.venus.core.service.category.ICategoryService;
+import com.dtopiast.venus.domain.base.MySpecification;
+import com.dtopiast.venus.domain.category.dto.*;
+import com.dtopiast.venus.domain.category.model.Category;
+
+import java.util.List;
+
+public class TopicService  implements ICategoryService {
+    @Override
+    public Category addCourseInCategory(AddCourseInCategoryDto dto) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteCourseInCategory(DeleteCourseInCategoryDto dto) {
+        return null;
+    }
+
+    @Override
+    public Category createCategory(CreateCategoryDto dto) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteCategory(DeleteCategoryDto dto) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategoryName(UpdateCategoryNameDto dto) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return null;
+    }
+
+    @Override
+    public <T extends MySpecification<Category>> List<Category> getAllCategoriesBySpecification(T specification) {
+        return null;
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return null;
+    }
+}
