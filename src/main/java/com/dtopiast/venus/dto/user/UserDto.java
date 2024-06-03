b3797175e682e482b5029b6ab07dab52f303a77b
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-06-02 19:47:19 -0600

    Fix responses property type in UserDto

--- src/main/java/com/dtopiast/venus/dto/user/UserDto.java
+++ src/main/java/com/dtopiast/venus/dto/user/UserDto.java
@@ -1,9 +1,7 @@
 package com.dtopiast.venus.dto.user;
 
-import com.dtopiast.venus.dto.response.ResponseDto;
-
 import java.util.List;
 
-public record UserDto(List<ResponseDto> responses, String name, String email, List<Long> profiles, List<Long> roles) {
+public record UserDto(List<Long> responses, String name, String email, List<Long> profiles, List<Long> roles) {
 
 }
