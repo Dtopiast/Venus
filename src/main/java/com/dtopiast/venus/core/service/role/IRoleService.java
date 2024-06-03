95aae063cf70973da73d78a5418601e0a9bce0c4
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-06-03 13:48:57 -0600

    Add service interface for roles

---
+++ src/main/java/com/dtopiast/venus/core/service/role/IRoleService.java
@@ -0,0 +1,7 @@
+package com.dtopiast.venus.core.service.role;
+
+import com.dtopiast.venus.core.service.base.MyService;
+import com.dtopiast.venus.core.service.response.IResponseManagementService;
+
+public interface IRoleService extends IRoleQueryService, IResponseManagementService,IRoleUserService {
+}
