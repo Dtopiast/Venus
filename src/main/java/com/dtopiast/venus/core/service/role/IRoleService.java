5dca1f8b6f8557479be1f45d1173078ac8319027
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2024-06-04 18:51:22 -0600

    Change name of createUser to registerUser

--- src/main/java/com/dtopiast/venus/core/service/role/IRoleService.java
+++ src/main/java/com/dtopiast/venus/core/service/role/IRoleService.java
@@ -3,5 +3,5 @@ package com.dtopiast.venus.core.service.role;
 import com.dtopiast.venus.core.service.base.MyService;
 import com.dtopiast.venus.core.service.response.IResponseManagementService;
 
-public interface IRoleService extends IRoleQueryService, IResponseManagementService,IRoleUserService {
+public interface IRoleService extends IRoleQueryService, IRoleManagementService,IRoleUserService {
 }
